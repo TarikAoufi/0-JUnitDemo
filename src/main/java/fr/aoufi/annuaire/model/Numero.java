90b1c8cf311b000e1074947e67a1990d50ced32a
Tarik AOUFI <t_aoufi@hotmail.com>
2016-08-22 11:18:29 +0200

    update

--- src/main/java/fr/aoufi/annuaire/model/Numero.java
+++ src/main/java/fr/aoufi/annuaire/model/Numero.java
@@ -82,36 +82,4 @@ public class Numero implements Serializable {
 		return "Numero [id=" + id + ", tel=" + tel + ", type=" + type + ", personnes=" + personnes + "]";
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((tel == null) ? 0 : tel.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Numero other = (Numero) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (tel == null) {
-			if (other.tel != null)
-				return false;
-		} else if (!tel.equals(other.tel))
-			return false;
-		return true;
-	}
-	
-		
 }
--- src/main/java/fr/aoufi/annuaire/model/Personne.java
+++ src/main/java/fr/aoufi/annuaire/model/Personne.java
@@ -107,46 +107,4 @@ public class Personne implements Serializable {
 				+ "]";
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dateNaissance == null) ? 0 : dateNaissance.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Personne other = (Personne) obj;
-		if (dateNaissance == null) {
-			if (other.dateNaissance != null)
-				return false;
-		} else if (!dateNaissance.equals(other.dateNaissance))
-			return false;
-		if (id != other.id)
-			return false;
-		if (nom == null) {
-			if (other.nom != null)
-				return false;
-		} else if (!nom.equals(other.nom))
-			return false;
-		if (prenom == null) {
-			if (other.prenom != null)
-				return false;
-		} else if (!prenom.equals(other.prenom))
-			return false;
-		return true;
-	}
-		
-	
-
 }
